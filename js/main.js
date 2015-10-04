b9bf4cd6fd5f65fbc93f5680ef0c980f3acf390e
victorgichohi <vitorgichohi48@gmail.com>
2015-10-04 17:12:28 +0300

    added the scrollig animation js

--- js/main.js
+++ js/main.js
@@ -28,3 +28,22 @@
 
         chart.draw(data, options);
       }
+
+// back to top button
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('a.back-to-top').fadeIn('slow');
+  } else {
+    $('a.back-to-top').fadeOut('slow');
+  }
+});
+
+// annimated scroll
+$('a.back-to-top').click(function() {
+  $('body, html').animate({
+    scrollTop: 0
+  }, 700);
+  return false;
+});
